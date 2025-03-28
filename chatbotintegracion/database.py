cee3bed9eb7af6a3182b4a7fc84214e650fc1f4f
Yamfer <u21100197@unimilitar.edu.co>
2025-03-27 21:06:46 -0500

    apirender2

--- chatbotintegracion/database.py
+++ chatbotintegracion/database.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 # Conectar a MongoDB
 MONGO_URI = os.getenv("MONGO_URI")
-client = MongoClient(MONGO_URI)
+client = MongoClient(MONGO_URI, tls=True, tlsCAFile=None)
 db = client["ClusterApi"]
 
 # GridFS para almacenamiento de archivos
