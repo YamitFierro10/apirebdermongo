b1c553448be2e5f6d0ae2f94b4ffabe0c190efa6
Yamfer <u21100197@unimilitar.edu.co>
2025-11-30 12:24:51 -0500

    apirender2

--- chatbotintegracion/database.py
+++ chatbotintegracion/database.py
@@ -1,35 +1,51 @@
+# from pymongo import MongoClient
+# import gridfs
+# import os
+# from dotenv import load_dotenv
+# import certifi
+
+# # Cargar variables de entorno
+# load_dotenv()
+
+# # Conectar a MongoDB
+# MONGO_URI = os.getenv("MONGO_URI")
+# client = MongoClient(MONGO_URI, tlsCAFile=certifi.where())
+# client_db = client["chatbot_db"]
+
+# # GridFS para almacenamiento de archivos
+# fs = gridfs.GridFS(client_db)
+
+# # Colección para historiales de conversación
+# collection = client_db["conversaciones"]
+
+# def guardar_archivo(ruta_archivo, nombre_archivo):
+#     """Guarda un archivo en MongoDB GridFS"""
+#     with open(ruta_archivo, "rb") as archivo:
+#         archivo_id = fs.put(archivo, filename=nombre_archivo)
+#     print(f"Archivo {nombre_archivo} guardado con ID {archivo_id}")
+#     return archivo_id
+
+# def obtener_archivo(nombre_archivo):
+#     """Obtiene un archivo desde MongoDB GridFS"""
+#     archivo = fs.find_one({"filename": nombre_archivo})
+#     if archivo:
+#         return archivo.read()  # Devuelve el contenido del archivo
+#     return None
+
+
 from pymongo import MongoClient
-import gridfs
-import os
-from dotenv import load_dotenv
 import certifi
+import os
 
-# Cargar variables de entorno
-load_dotenv()
-
-# Conectar a MongoDB
 MONGO_URI = os.getenv("MONGO_URI")
-client = MongoClient(MONGO_URI, tlsCAFile=certifi.where())
+client = MongoClient(MONGO_URI, tls=True, tlsCAFile=certifi.where())
 client_db = client["chatbot_db"]
 
-# GridFS para almacenamiento de archivos
-fs = gridfs.GridFS(client_db)
-
-# Colección para historiales de conversación
 collection = client_db["conversaciones"]
 
-def guardar_archivo(ruta_archivo, nombre_archivo):
-    """Guarda un archivo en MongoDB GridFS"""
-    with open(ruta_archivo, "rb") as archivo:
-        archivo_id = fs.put(archivo, filename=nombre_archivo)
-    print(f"Archivo {nombre_archivo} guardado con ID {archivo_id}")
-    return archivo_id
+try:
+    client.admin.command("ping")
+    print("✅ Conectado a MongoDB correctamente")
+except Exception as e:
+    print("❌ Error:", e)
 
-def obtener_archivo(nombre_archivo):
-    """Obtiene un archivo desde MongoDB GridFS"""
-    archivo = fs.find_one({"filename": nombre_archivo})
-    if archivo:
-        return archivo.read()  # Devuelve el contenido del archivo
-    return None
-
-#otro
