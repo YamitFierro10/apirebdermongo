c95b90ab350168aff5eae5abc33c4e7dac395b72
Yamfer <u21100197@unimilitar.edu.co>
2025-11-30 13:24:13 -0500

    apirender2

--- chatbotintegracion/database.py
+++ chatbotintegracion/database.py
@@ -24,17 +24,17 @@ try:
 except Exception as e:
     print("❌ Error:", e)
 
-# def guardar_archivo(ruta_archivo, nombre_archivo):
-#     """Guarda un archivo en MongoDB GridFS"""
-#     with open(ruta_archivo, "rb") as archivo:
-#         archivo_id = fs.put(archivo, filename=nombre_archivo)
-#     print(f"Archivo {nombre_archivo} guardado con ID {archivo_id}")
-#     return archivo_id
+def guardar_archivo(ruta_archivo, nombre_archivo):
+    """Guarda un archivo en MongoDB GridFS"""
+    with open(ruta_archivo, "rb") as archivo:
+        archivo_id = fs.put(archivo, filename=nombre_archivo)
+    print(f"Archivo {nombre_archivo} guardado con ID {archivo_id}")
+    return archivo_id
 
-# def obtener_archivo(nombre_archivo):
-#     """Obtiene un archivo desde MongoDB GridFS"""
-#     archivo = fs.find_one({"filename": nombre_archivo})
-#     if archivo:
-#         return archivo.read()  # Devuelve el contenido del archivo
-#     return None
+def obtener_archivo(nombre_archivo):
+    """Obtiene un archivo desde MongoDB GridFS"""
+    archivo = fs.find_one({"filename": nombre_archivo})
+    if archivo:
+        return archivo.read()  # Devuelve el contenido del archivo
+    return None
 
